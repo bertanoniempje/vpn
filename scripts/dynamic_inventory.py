78840f30e7df4c6ba3738a15f81fbe7be5287c80
rob <bertanoniempje@proton.me>
2024-05-24 20:51:02 +0200

    changed /dynamic_inventory.py

--- scripts/dynamic_inventory.py
+++ scripts/dynamic_inventory.py
@@ -1,9 +1,43 @@
-[WARNING]:  * Failed to parse /home/rob/vpn/scripts/dynamic_inventory.py with script plugin: Invalid empty host name provided:
-[WARNING]:  * Failed to parse /home/rob/vpn/scripts/dynamic_inventory.py with ini plugin: /home/rob/vpn/scripts/dynamic_inventory.py:3: Expected
-key=value host variable assignment, got: os
-[WARNING]: Unable to parse /home/rob/vpn/scripts/dynamic_inventory.py as an inventory source
-[WARNING]: No inventory was parsed, only implicit localhost is available
-[WARNING]: provided hosts list is empty, only localhost is available. Note that the implicit localhost does not match 'all'
+#!/usr/bin/env python3
 
-PLAY [Pre-tasks for setting up VPN server] *********************************************************************************************************
-skipping: no hosts matched
+import os
+import json
+
+# Get environment variables
+rpi_ip = os.getenv('RPI_IP')
+rpi_user = os.getenv('RPI_USER')
+client_ip = os.getenv('CLIENT_IP')
+client_user = os.getenv('CLIENT_USER')
+
+# Construct inventory dictionary
+inventory = {
+    'all': {
+        'hosts': [],
+        'children': {
+            'raspberry_pi': {
+                'hosts': [],
+                'vars': {
+                    'ansible_user': rpi_user
+                }
+            },
+            'client': {
+                'hosts': [],
+                'vars': {
+                    'ansible_user': client_user
+                }
+            }
+        }
+    }
+}
+
+# Add Raspberry Pi and client IP addresses to the inventory
+if rpi_ip:
+    inventory['all']['hosts'].append(rpi_ip)
+    inventory['all']['children']['raspberry_pi']['hosts'].append(rpi_ip)
+
+if client_ip:
+    inventory['all']['hosts'].append(client_ip)
+    inventory['all']['children']['client']['hosts'].append(client_ip)
+
+# Print the inventory as JSON
+print(json.dumps(inventory))
