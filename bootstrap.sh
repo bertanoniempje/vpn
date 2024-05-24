c5bdb3fc5086835cd62ffd98a29e96b578b6223c
rob <bertanoniempje@proton.me>
2024-05-24 15:27:01 +0200

    added pre_tasks playbook and removed apt upgrade from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #update & upgrade
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y
 
 #install ansible and git
 sudo apt install ansible -y
@@ -10,8 +10,11 @@ sudo apt install git -y
 #clone ansible playbook
 git clone https://github.com/bertanoniempje/vpn.git $HOME/vpn
 
+#run pre_tasks playbook
+cd $HOME/vpn && ansible-playbook ansible-playbook -i inventories/pi/hosts playbooks/pre_tasks.yml
+
 #run playbook to install wireguard on vpn server
-cd $HOME/vpn && ansible-playbook ansible-playbook -i inventories/pi/hosts playbooks/install_wireguard.yml 
+ansible-playbook ansible-playbook -i inventories/pi/hosts playbooks/install_wireguard.yml
 
 #run playbook to add new client to the vpn server
 ansible-playbook -i inventories/pi/hosts playbooks/add_client.yml
